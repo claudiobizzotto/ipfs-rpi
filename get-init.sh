14af36a434355a0be2b3aadc1c93fde8714c5ebf
claudiobizzotto <it@claudiobizzotto.com>
2017-12-13 22:08:36 -0500

    Wooops

--- get-init.sh
+++ get-init.sh
@@ -12,7 +12,7 @@ function get_init_system() {
       init_from_package_manager=$(sudo dpkg -S /sbin/init | cut -d ":" -f 1) ||
       init_from_package_manager=$(sudo rpm -qf /sbin/init)
 
-    [[ $init_from_process == *"systemd"* ]] && echo "systemd" && return 1
+    [[ $init_from_package_manager == *"systemd"* ]] && echo "systemd" && return 1
     [[ $init_from_package_manager == *"upstart"* ]] && echo "upstart" && return 1
   fi
 
